808fab19ab7dc1f8f8571178fa2fbc3cf6f3d877
Marcus <marcus.henriksson@restory.ai>
2023-10-17 17:14:07 +0200

    fix

--- password.js
+++ password.js
@@ -1,7 +1,9 @@
 function validatePassword(password) {
-    //Lösning med REGEX-Expression
+    //Kontroll med REGEX-Expression
     const passwordRegex = /^(?=.*[0-9])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=.{8,})/;
     return passwordRegex.test(password);
+
+    //Funktionen returnerar 'true' om password uppfyller Regex expression
 }
 
 module.exports = validatePassword;
